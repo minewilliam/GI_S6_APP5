af062b08a435a0f6941fa05a7303a4201bd8bd99
assassimb <assassimb@gmail.com>
2022-07-06 16:13:47 -0400

    debut du scanner

--- ArgonDevice/src/ArgonDevice.cpp
+++ ArgonDevice/src/ArgonDevice.cpp
@@ -3,17 +3,23 @@
 /******************************************************/
 
 #include "Particle.h"
-#line 1 "d:/UDES/S6/APP5/GI_S6_APP5/ArgonDevice/src/ArgonDevice.ino"
+#line 1 "/home/assim/Documents/S6-APP5/GI_S6_APP5/ArgonDevice/src/ArgonDevice.ino"
+#include "BeaconScanner.h"
+
 void setup();
 bool switchLED(String extra);
+void onCallback(Beacon& beacon, callback_type type);
 void loop();
-#line 1 "d:/UDES/S6/APP5/GI_S6_APP5/ArgonDevice/src/ArgonDevice.ino"
+#line 3 "/home/assim/Documents/S6-APP5/GI_S6_APP5/ArgonDevice/src/ArgonDevice.ino"
 bool status = false;
 const pin_t MY_LED = D7;
-//GET https://api.particle.io/v1/devices/E00FCE68986801735C908D66/temp?access_token=9bf9cd2bbb783ae5a966e1eea48c4f568ce4da29
+SYSTEM_THREAD(ENABLED);
 
 void setup()
 {
+  BLE.on();
+  Scanner.startContinuous();
+  Scanner.setCallback(onCallback);
   pinMode(D7, OUTPUT);
   Particle.variable("status", status);
   Particle.function("LED", switchLED);
@@ -24,9 +30,16 @@ bool switchLED(String extra) {
   return status;
 }
 
+void onCallback(Beacon& beacon, callback_type type) {
+  Log.trace("Address: %s. Type: %s", beacon.getAddress().toString().c_str(), 
+    (type == NEW) ? "Entered": "Left");
+  Serial.print(beacon.getAddress().toString().c_str());
+}
+
 void loop()
 {
+  Scanner.loop();
   digitalWrite(MY_LED, status);
-  if(status) Serial.print("we gucc");
+  Serial.print("ca marche la");
   delay(2500);
 }
--- ArgonDevice/src/ArgonDevice.ino
+++ ArgonDevice/src/ArgonDevice.ino
@@ -1,8 +1,14 @@
+#include "BeaconScanner.h"
+
 bool status = false;
 const pin_t MY_LED = D7;
+SYSTEM_THREAD(ENABLED);
 
 void setup()
 {
+  BLE.on();
+  Scanner.startContinuous();
+  Scanner.setCallback(onCallback);
   pinMode(D7, OUTPUT);
   Particle.variable("status", status);
   Particle.function("LED", switchLED);
@@ -13,9 +19,16 @@ bool switchLED(String extra) {
   return status;
 }
 
+void onCallback(Beacon& beacon, callback_type type) {
+  Log.trace("Address: %s. Type: %s", beacon.getAddress().toString().c_str(), 
+    (type == NEW) ? "Entered": "Left");
+  Serial.print(beacon.getAddress().toString().c_str());
+}
+
 void loop()
 {
+  Scanner.loop();
   digitalWrite(MY_LED, status);
-  if(status) Serial.print("we gucc");
+  Serial.print("ca marche la");
   delay(2500);
 }
